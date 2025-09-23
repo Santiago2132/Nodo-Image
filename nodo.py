40b6a18fa41b12433efde360d1ced0379b46d849
Santiago Maldonado Rojas <santiagomaldonadorojas64@gmail.com>
2025-09-23 14:48:23 -0500

    Cambio de ip

--- nodo.py
+++ nodo.py
@@ -10,7 +10,7 @@ import socket
 import requests
 import json
 
-BALANCEADOR = "http://127.0.1.1:5000/api/nodos/registrar"  # Cambiar por la URL real
+BALANCEADOR = "http://192.168.154.129:5000/api/nodos/registrar"  # Cambiar por la URL real
 
 class GestorNodos:
     def __init__(self):
